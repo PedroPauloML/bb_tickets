395e7085b1dd85b16205d389c385c77ec1bcfd2f
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2021-12-27 13:56:33 -0300

    Generate layout

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A documentação oficial para uso da API está disponível nesse [link](https://
 Para funcionamento do serviço, alguns dependências são necessárias. Dentre elas:
 
 1. [HTTParty](https://github.com/jnunemaker/httparty) (requisições HTTP);
+1. [BRCobranca](https://github.com/kivanio/brcobranca) (Emissão de bloquetos de cobrança para bancos brasileiros);
 
 ## Como usar?
 
@@ -17,6 +18,37 @@ O serviço é disponibilizado através do arquivo `bb_tickets`. Nele é disponib
 1. Autenticação;
 2. Registro (create);
 3. Listagem (index);
-4. Visualização (show).
+4. Visualização (show);
+5. Baixa/cancelamento (destroy);
+6. Geração do layout do boleto (generate_layout).
 
 Para auxiliar no uso, está disponível testes no diretório `/examples`.
+
+## Coreção de erros
+
+1. RGhost::RenderException: Error: /invalidfileaccess in --run--
+
+   **Descrição do erro**
+
+   ```sh
+     RGhost::RenderException: Error: /invalidfileaccess in --run--
+     Operand stack:
+         (/Users/pedropaulomarquesdelima/.rvm/gems/ruby-2.7.2/gems/brcobranca-9.2.4/lib/brcobranca/boleto/template/../../arquivos/templates/modelo_generico.eps)   (r)
+     Execution stack:
+         %interp_exit   .runexec2   --nostringval--   run   --nostringval--   2   %stopped_push   --nostringval--   run   run   false   1   %stopped_push   1990   1   3   %oparray_pop   1989   1   3   %oparray_pop   1977   1   3   %oparray_pop   1833   1   3   %oparray_pop   --nostringval--   %errorexec_pop   .runexec2   --nostringval--   run   --nostringval--   2   %stopped_push   --nostringval--   run   1990   1   3   %oparray_pop   run
+     Dictionary stack:
+         --dict:732/1123(ro)(G)--   --dict:0/20(G)--   --dict:244/300(L)--
+     Current allocation mode is local
+     Last OS error: Permission denied
+     Current file position is 3917
+
+     from /Users/pedropaulomarquesdelima/.rvm/gems/ruby-2.7.2/gems/rghost-0.9.7/lib/rghost/ruby_ghost_engine.rb:88:in `render'
+   ```
+
+   **Solução**
+
+   Esse erro foi corrigido no commit `https://github.com/kivanio/brcobranca/commit/c5bb8ab493bee3bc072e857fd8629673e911aba6`. Mas, caso o erro persista, insira a linha de código abaixo no arquivo `/path/to/gems/brcobranca-9.2.4/lib/brcobranca/boleto/template/rghost.rb`, na linha 27.
+
+   ```ruby
+     RGhost::Config::GS[:default_params] << '-dNOSAFER'
+   ```
--- bb_tickets.rb
+++ bb_tickets.rb
@@ -1,5 +1,8 @@
 require 'httparty'
 require 'uri'
+require 'brcobranca'
+require 'date'
+require 'tempfile'
 
 class BBTickets
   attr_accessor(:verbose)
@@ -163,7 +166,7 @@ class BBTickets
 
     params[:numeroConvenio] ||= @numero_convenio
 
-    validate_params_to_create(params)
+    self.class.validate_params_to_create(params)
 
     verbose_output('Creating', 'starting')
 
@@ -205,6 +208,187 @@ class BBTickets
     end
 
     response
+  end
+
+  def self.generate_layout(params)
+    raise "Params should be a Hash" unless params.class == Hash
+
+    params = JSON.parse(params.to_json, symbolize_names: true)
+
+    self.validate_params_to_create(params)
+
+    # To more details, read this page https://rubydoc.info/github/kivanio/brcobranca/Brcobranca/Boleto/Base
+    layout = Brcobranca::Boleto::BancoBrasil.new
+
+    # REQUERIDO: Informa se o banco deve aceitar o boleto após o vencimento ou não( S ou N, quase sempre S)
+    layout.aceite = params[:codigoAceite]
+
+    # REQUERIDO: Número da agencia sem Digito Verificador
+    layout.agencia = '452'
+
+    # OPCIONAL: Nome do avalista
+    layout.avalista = params[:beneficiarioFinal][:nome]
+
+    # OPCIONAL: Documento do avalista
+    layout.avalista_documento = params[:beneficiarioFinal][:numeroInscricao]
+
+    # REQUERIDO: Carteira utilizada
+    layout.carteira = params[:numeroCarteira]
+
+    # OPCIONAL: Variacao da carteira(opcional para a maioria dos bancos)
+    layout.carteira_label = params[:numeroVariacaoCarteira]
+
+    # REQUERIDO: Nome do beneficiário
+    layout.cedente = params[:beneficiarioFinal][:nome]
+
+    # OPCIONAL: Endereço do beneficiário
+    layout.cedente_endereco = 'Av. Estreita do Largo, 123, São Paulo/SP'
+
+    # OPCIONAL: Código utilizado para identificar o tipo de serviço cobrado
+    # layout.codigo_servico =
+
+    # REQUERIDO: Número da conta corrente sem Digito Verificador
+    layout.conta_corrente = '123873'
+
+    # REQUERIDO: Número do convênio/contrato do cliente junto ao banco emissor
+    layout.convenio = params[:numeroConvenio]
+
+    # REQUERIDO: Data de pedido, Nota fiscal ou documento que originou o boleto
+    # layout.data_documento = params[:']
+
+    # OPCIONAL: Data de processamento do boleto
+    # layout.data_processamento = params[:']
+
+    # REQUERIDO: Data de vencimento do boleto
+    layout.data_vencimento = Date.parse(params[:dataVencimento].gsub('.', '/'))
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao sacado
+    # layout.demonstrativo =
+
+    # REQUERIDO: Documento do beneficiário (CPF ou CNPJ)
+    layout.documento_cedente = params[:beneficiarioFinal][:numeroInscricao]
+
+    # OPCIONAL: Número de pedido, Nota fiscal ou documento que originou o boleto
+    # layout.documento_numero =
+
+    # REQUERIDO: Símbolo da moeda utilizada (R$ no brasil)
+    layout.especie = 'R$'
+
+    # REQUERIDO: Tipo do documento (Geralmente DM que quer dizer Duplicata Mercantil)
+    layout.especie_documento = params[:descricaoTipoTitulo] || 'DM'
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao1 = params[:mensagemBloquetoOcorrencia]
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao2 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao3 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao4 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao5 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao6 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    # layout.instrucao7 = params[:']
+
+    # OPCIONAL: Utilizado para mostrar alguma informação ao caixa
+    layout.instrucoes = params[:mensagemBloquetoOcorrencia]
+
+    # REQUERIDO: Informação sobre onde o sacado podera efetuar o pagamento
+    layout.local_pagamento = 'QUALQUER BANCO OU LOTÉRICA ATÉ O VENCIMENTO'
+
+    # REQUERIDO: Tipo de moeda utilizada (Real(R$) e igual a 9)
+    layout.moeda = '9'
+
+    # OPCIONAL: Número sequencial utilizado para identificar o boleto
+    layout.nosso_numero = params[:numeroTituloCliente][-10..-1]
+
+    # REQUERIDO: Quantidade de boleto(padrão = 1)
+    layout.quantidade = '1'
+
+    # REQUERIDO: Nome do pagador
+    layout.sacado = params[:pagador][:nome]
+
+    # REQUERIDO: Documento do pagador
+    layout.sacado_documento = params[:pagador][:numeroInscricao]
+
+    # OPCIONAL: Endereco do pagador
+    # layout.sacado_endereco = params[:pagador][:endereco]
+
+    # REQUERIDO: Valor do boleto
+    layout.valor = params[:valorOriginal]
+
+    # OPCIONAL: Rótulo da Carteira, RG ou SR, somente para impressão no boleto.
+    # layout.variacao =
+
+    # ticket_layout = layout.to(:pdf)
+
+    # ticket_layout
+    layout
+  end
+
+  def self.validate_params_to_create(params)
+    required_params = %i(
+      numeroConvenio
+      dataVencimento
+      valorOriginal
+      numeroCarteira
+      numeroVariacaoCarteira
+      codigoModalidade
+      dataEmissao
+      codigoAceite
+      codigoTipoTitulo
+      indicadorPermissaoRecebimentoParcial
+      numeroTituloCliente
+      pagador
+      beneficiarioFinal
+    )
+    nested_required_params = {
+      pagador: %i(
+        tipoInscricao
+        numeroInscricao
+        nome
+        endereco
+        cep
+        cidade
+        bairro
+        uf
+      ),
+      beneficiarioFinal: %i(
+        tipoInscricao
+        numeroInscricao
+        nome
+      )
+    }
+
+    params_not_present = required_params - params.keys
+
+    present_nested_attributes = (
+      nested_required_params.keys - params_not_present
+    )
+    nested_params_not_present = []
+    if present_nested_attributes.count > 0
+      present_nested_attributes.each do |key|
+        not_present = nested_required_params[key] - params[key].keys
+        not_present.each { |nested_params_key|
+          nested_params_not_present.push(:"#{key}.#{nested_params_key}")
+        }
+      end
+    end
+
+    params_not_present.push(nested_params_not_present)
+    params_not_present.flatten!
+
+    if params_not_present.count > 0
+      raise "Required params was not informed: #{params_not_present.join(', ')}"
+    end
   end
 
   private
@@ -283,63 +467,6 @@ class BBTickets
   def check_authentication
     unless @access_token_expires_at and Time.now < @access_token_expires_at
       authenticate
-    end
-  end
-
-  def validate_params_to_create(params)
-    required_params = %i(
-      numeroConvenio
-      dataVencimento
-      valorOriginal
-      numeroCarteira
-      numeroVariacaoCarteira
-      codigoModalidade
-      dataEmissao
-      codigoAceite
-      codigoTipoTitulo
-      indicadorPermissaoRecebimentoParcial
-      numeroTituloCliente
-      pagador
-      beneficiarioFinal
-    )
-    nested_required_params = {
-      pagador: %i(
-        tipoInscricao
-        numeroInscricao
-        nome
-        endereco
-        cep
-        cidade
-        bairro
-        uf
-      ),
-      beneficiarioFinal: %i(
-        tipoInscricao
-        numeroInscricao
-        nome
-      )
-    }
-
-    params_not_present = required_params - params.keys
-
-    present_nested_attributes = (
-      nested_required_params.keys - params_not_present
-    )
-    nested_params_not_present = []
-    if present_nested_attributes.count > 0
-      present_nested_attributes.each do |key|
-        not_present = nested_required_params[key] - params[key].keys
-        not_present.each { |nested_params_key|
-          nested_params_not_present.push(:"#{key}.#{nested_params_key}")
-        }
-      end
-    end
-
-    params_not_present.push(nested_params_not_present)
-    params_not_present.flatten!
-
-    if params_not_present.count > 0
-      raise "Required params was not informed: #{params_not_present.join(', ')}"
     end
   end
 end
